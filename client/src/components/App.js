d57100eaf757a755f7b858e0653f656077050bfc
cphung3 <cphung@mit.edu>
2019-01-17 16:53:31 -0500

    Replace skeleton

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,15 @@
 import React from "react";
-import "../css/app.css"
+import "../css/app.css";
 import Route from "react-router-dom/es/Route";
-import Switch from "react-router-dom/es/Switch";
-import GameContainer from "./GameContainer";
-import GameRules from "./GameRules";
+import Switch from "react-router-dom/es/Switch"
+import Root from "./Root"
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path="/" component={GameContainer} />
-          <Route exact path="/rules" component={GameRules} />
+          <Route exact path="/" component={Root} />
         </Switch>
       </div>
     )
